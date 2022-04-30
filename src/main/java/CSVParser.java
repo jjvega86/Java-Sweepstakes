02fa1f7de8b9380c4b4a7116e0bf4c667405f87b
JJ <jj.vega86@gmail.com>
2022-04-30 12:56:27 -0400

    Added CSVParser class to project with Parse method

---
+++ src/main/java/CSVParser.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class CSVParser {
+	
+	public ArrayList<Contestant> Parse(){
+		ArrayList<Contestant> parsedContestants = new ArrayList<Contestant>();
+		return parsedContestants;
+		
+	}
+
+}
--- src/main/java/Contestant.java
+++ src/main/java/Contestant.java
@@ -1,4 +1,52 @@
 
 public class Contestant {
+	public int id;
+	public String firstName;
+	public String lastName;
+	public String emailAddress;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public Contestant() {
+		super();
+	}
+
+	public Contestant(int id, String firstName, String lastName, String emailAddress) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailAddress = emailAddress;
+	}
 
 }
