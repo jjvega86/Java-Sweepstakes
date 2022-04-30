3b3861fbb0de58721ac051d0eb0ae78085895182
JJ <jj.vega86@gmail.com>
2022-04-30 13:48:15 -0400

    Added MarketingFirm class and wrote logic for AddSweepstakes and RunSweepstakes

---
+++ src/main/java/MarketingFirm.java
@@ -0,0 +1,22 @@
+
+public class MarketingFirm {
+	SweepstakesManager manager;
+
+	public MarketingFirm(SweepstakesManager manager) {
+		super();
+		this.manager = manager;
+	}
+
+	public void AddSweepstakes(String name) {
+		Sweepstakes newSweepstakes = new Sweepstakes(name);
+		manager.InsertSweepstakes(newSweepstakes);
+	}
+
+	public void RunSweepstakes() {
+		Sweepstakes runningSweepstakes = manager.GetSweepstakes();
+		Contestant winner = runningSweepstakes.PickWinner();
+		Helpers.Print("The winner is " + winner.firstName + "!");
+		//TODO: Use observer pattern to send a message to all contestants based on winner
+	}
+
+}
